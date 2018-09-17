cd9f443d4ba4d29cd039e672fb5bdaeb1c92cf42
daiminglei <516843761@qq.com>
2018-09-17 17:36:52 +0800

    1.submit

---
+++ xcx_cms/index.php
@@ -0,0 +1,7 @@
+<?php
+define('IMG_URL','https://'.$_SERVER['HTTP_HOST'].'/attachs/uploads/');
+
+define('APP_PATH', __DIR__ . '/youge/');
+//手机验证测试阶段；
+define('IS_ONLINE',true);
+require __DIR__ . '/thinkphp/start.php';
